4c3936542b9f7738c50ef19d5ca16b86ed29c69c
ShreeChem <s.madhushree25@gmail.com>
2026-09-14 15:25:57 +0200

    Add PWA service worker

---
+++ public/sw.js
@@ -0,0 +1,5 @@
+const CACHE='engineering-delivery-flow-v22-public-safe';
+const ASSETS=['./','./index.html','./app.html','./styles.css','./app.js','./icon.svg','./manifest.webmanifest'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{if(new URL(e.request.url).origin===location.origin){const c=r.clone();caches.open(CACHE).then(x=>x.put(e.request,c));}return r;}).catch(()=>caches.match(e.request).then(hit=>hit||caches.match('./app.html'))));});
